07c31472b07ca98ca86ee0b26ab8bd7189f30fe9
ppdx999 <ppdx999@gmail.com>
2023-02-21 00:10:39 +0900

    (typescript/main) make repl that just echo the input.

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,17 @@
-import { fn } from "./fn";
+import { createInterface } from "readline";
 
-console.log(fn());
+const reader = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.on("line", (line) => {
+  console.log(line);
+  process.stdout.write("> ");
+});
+
+reader.on("close", () => {
+  console.log("bye!");
+});
+
+process.stdout.write("> ");
